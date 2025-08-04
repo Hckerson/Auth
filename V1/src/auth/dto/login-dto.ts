3c306059f180c42db04ee804e6b434c4aad1dfd1
Hckerson <166761968+Hckerson@users.noreply.github.com>
2025-08-04 22:47:21 +0100

    Update file permissions to executable for various configuration and documentation files; remove unused authentication-related files to streamline the codebase.

--- V1/src/auth/dto/login-dto.ts
+++
@@ -1,9 +0,0 @@
-import { SignUpDto } from "./signup-dto";
-import { PartialType } from "@nestjs/mapped-types";
-
-
-export class LoginDto extends PartialType(SignUpDto) {
-  rememberMe?: boolean; // Optional field for "Remember Me" functionality
-  twoFactorCode?: string; // Optional field for two-factor authentication 
-  ipAddress?: string; // Optional field for capturing the user's IP address
-}
--- V1/src/lib/gen-token.ts
+++
@@ -1,5 +0,0 @@
-import { randomBytes } from "node:crypto";
-
-export function genToken() {
-  return randomBytes(32).toString('hex');
-}
